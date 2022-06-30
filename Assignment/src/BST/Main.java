6ce03d7b14c86330a42fea33dffde4300eeecfee
Saurabh070699 <89467323+Saurabh070699@users.noreply.github.com>
2022-06-30 20:13:59 +0530

    Add files via upload

---
+++ Assignment/src/BST/Main.java
@@ -0,0 +1,11 @@
+package BST;
+
+import DSA.CONSTRUCTION;
+
+public class Main {
+	public static void main(String[] args) {
+        CONSTRUCTION construction = new CONSTRUCTION();
+        construction.getIData();
+        construction.orderCalculator(construction.floorSize);
+    }
+}
